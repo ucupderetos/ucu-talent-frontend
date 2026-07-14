30b865853be4b7a7cd59107dc622c78cef9a29b9
Felipe Corderi <felipe@corderi.com>
2026-07-14 12:08:04 -0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center">
+        <h1 className="text-3xl font-semibold">UCU Talent, Coming soon...</h1>
+      </main>
+    </div>
+  );
+}
